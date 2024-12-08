263e5dccfbff52010fc8f157b1ac6d65cc920e2b
MashobTechie <sheriffdeenmakinde@gmail.com>
2024-12-08 09:10:32 +0000

    State Management

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -28,7 +28,7 @@ function Counter() {
                         ? "Today is "
                         : count > 0
                             ? `${count} days from today is `
-                            : `${Math.abs(count)} days ago was `}
+                            : `${Math.abs(count)} days ago `}
                 </span>
                 <span>{date.toDateString()}</span>
             </p>
