b3fd2037c042760ce5222923238439d08e1ffe59
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-04-14 10:59:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css' // To import the appcss code. 
+
+function App() {
+ 
+  return (
+    <>
+      <p>Hello React</p>
+      <div>
+        <p>Hello World</p>
+        <p>Hello World</p>
+        <p>Hello World</p>
+      </div>
+    </>
+  )
+}
+
+
+// Export our app
+export default App;
