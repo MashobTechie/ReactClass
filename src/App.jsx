515913ad06a874954dba7997fbe89ffed46c6267
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-04-18 16:48:33 +0100

    Creating and resusing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import './App.css' // To import the appcss code. 
 import Home from './pages/Home';
 import About from './pages/About';
+import ProductCard from './components/ProductCard';
 
 function App() {
 
   return (
     <>
-      <Home/>
-      <About/>
 
+      {/* <Home/> */}
+      {/* <About/> */}
 
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </>
+
+
   )
 }
 
