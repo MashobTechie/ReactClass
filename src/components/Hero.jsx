d5f0ed7757997412029b6879e263ce60b83a9ac0
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-04-24 11:34:02 +0100

    handled the navbar hr

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        
+            <section className="hero bg-orange-100 h-screen flex flex-col justify-center items-center">
+                <div className="hero-content text-center">
+                    <h1 className="text-5xl font-bold text-green-700">Welcome to Our Restaurant</h1>
+                    
+                    
+                </div>
+
+            </section>
+        
+    )
+}
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Navbar = () => {
-    return (
+    return (<section className=' bg-orange-100'>
         <section className='flex justify-center items-center h-16 bg-orange-100'>
-            <nav className='flex justify-between items-center w-4/5 text-green-700  m-0 bg-transparent'>
+            <nav className='flex justify-between items-center w-4/5 text-green-700  m-0 bg-orange-100'>
                 <div className='flex items-center'>
                     <h2 className='text-2xl font-bold'>Logo</h2>
                 </div>
@@ -14,21 +14,34 @@ const Navbar = () => {
                         <li className='hover:underline cursor-pointer'>Services</li>
                         <li className='hover:underline cursor-pointer'>Contact</li>
                     </ul>
-
                 </div>
-
                 <div>
                     <button className=' bg-white rounded   text-green-600 '
-                    style={{
-                        'border': '2px solid green',
-                        'borderRadius': '24px',
-                        'width': '120px',
-                        'padding': '4px ',
-                    }}
+                        style={{
+                            'border': '2px solid green',
+                            'borderRadius': '24px',
+                            'width': '120px',
+                            'padding': '4px ',
+                        }}
                     >Sign Up</button>
                 </div>
             </nav>
         </section>
+        <hr className=' bg-black'
+            style={{
+                'height': '2px',
+                'border': 'none',
+
+                'width': '80%',
+                'marginTop': '0px',
+                'marginBottom': '0px',
+                'marginLeft': 'auto',
+                'marginRight': 'auto',
+            }} />
+
+
+    </section>
+
     )
 }
 
