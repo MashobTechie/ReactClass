cb54395698a3ce859e04a5254baab6523c312fda
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-04-24 12:03:14 +0100

    completed the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import PizzaImage from '../assets/premium_photo-1661762555601-47d088a26b50.avif'
 const Hero = () => {
     return (
         <section className="relative min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-16 py-20 overflow-hidden bg-orange-100 ">
-            <div className="hero-content text-center "  style={
-            {
-                marginTop: '-60px',
-            }
-        }>
+            <div className="hero-content text-center " style={
+                {
+                    marginTop: '-60px',
+                }
+            }>
                 <h1 className="text-5xl text-green-900 font-serif ">Dive Into Delicious <br />Meal Dishes</h1>
                 <div className="image-row flex flex-wrap justify-center gap-4" style={{
                     marginTop: '60px'
@@ -17,31 +17,31 @@ const Hero = () => {
                         borderRadius: '20px',
                         boxShadow: '0 8px 12px rgba(0, 0, 0, 0.5',
                         width: '200px',
-                         
+
 
                     }} />
                     <img src={PizzaImage} alt="Dish 2" className="h-32 object-cover"
-                    style={{
+                        style={{
+                            borderRadius: '20px',
+                            boxShadow: '0 8px 12px rgba(0, 0, 0, 0.5',
+                            width: '200px',
+
+
+                        }} />
+                    <img src={PizzaImage} alt="Dish 3" className="h-48 object-cover" style={{
                         borderRadius: '20px',
                         boxShadow: '0 8px 12px rgba(0, 0, 0, 0.5',
                         width: '200px',
 
 
                     }} />
-                    <img src={PizzaImage} alt="Dish 3" className="h-48 object-cover"  style={{
-                        borderRadius: '20px',
-                        boxShadow: '0 8px 12px rgba(0, 0, 0, 0.5',
-                        width: '200px',
-
-
-                    }}/>
-                    <img src={PizzaImage} alt="Dish 4" className="h-36 object-cover"style={{
+                    <img src={PizzaImage} alt="Dish 4" className="h-36 object-cover" style={{
                         borderRadius: '20px',
                         boxShadow: '0 8px 12px rgba(0, 0, 0, 0.5',
                         width: '180px',
 
 
-                    }}/>
+                    }} />
                 </div>
             </div>
         </section>
